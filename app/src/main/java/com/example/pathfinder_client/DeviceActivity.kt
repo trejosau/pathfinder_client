2bf53813d9fc60fea12c6a2938abf9f02cacc245
thenotoriousxul <saulsanchezlopez999@gmail.com>
2025-03-20 13:07:50 -0600

    Usando Git LFS para el video

---
+++ app/src/main/java/com/example/pathfinder_client/DeviceActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pathfinder_client
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
+import android.widget.TextView
+
+
+class DeviceActivity : AppCompatActivity() {
+
+}
